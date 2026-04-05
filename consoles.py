11492a761399f1a1af5f7d3b3448f42c846f28a1
Bonkary <zachary.simmons@proton.me>
2026-04-04 18:50:51 -0700

    im switching computers

--- consoles.py
+++ consoles.py
@@ -28,8 +28,6 @@ class ConsoleContainer(QFrame):
         
         rootLayout.addLayout(mainLayout)
 
-
-
 class Gameboy(QFrame):
     def __init__(self):
         super().__init__()
@@ -40,33 +38,43 @@ class Gameboy(QFrame):
         columnSpacing = 30
         # Row 1
         row1 = wdgts.NoPadHBoxLayout()
-        buttonA = KeyboardButtonInputs(name='A Button')
-        buttonB = KeyboardButtonInputs(name="B Button")
-        bumperR = KeyboardButtonInputs(name="Right Bumper")
-        bumperL = KeyboardButtonInputs(name="Left Bumper")
+        self._buttonA = KeyboardButtonInputs(name='A Button')
+        self._buttonB = KeyboardButtonInputs(name="B Button")
+        self._bumperR = KeyboardButtonInputs(name="Right Bumper")
+        self._bumperL = KeyboardButtonInputs(name="Left Bumper")
         
-        row1.addWidget(buttonA)
+        row1.addWidget(self._buttonA)
         row1.addSpacing(columnSpacing)
-        row1.addWidget(buttonB)
+        row1.addWidget(self._buttonB)
         row1.addSpacing(columnSpacing)
-        row1.addWidget(bumperR)
+        row1.addWidget(self._bumperR)
         row1.addSpacing(columnSpacing)
-        row1.addWidget(bumperL)
+        row1.addWidget(self._bumperL)
         
         # Row 2
         row2 = wdgts.NoPadHBoxLayout()
-        dpadUp = KeyboardButtonInputs(name="D-Pad Up")
-        dpadDown = KeyboardButtonInputs(name="D-Pad Down")
-        dpadLeft = KeyboardButtonInputs(name="D-Pad Left")
-        dpadRight = KeyboardButtonInputs(name="D-Pad Right")
+        self._dpadUp = KeyboardButtonInputs(name="D-Pad Up")
+        self._dpadDown = KeyboardButtonInputs(name="D-Pad Down")
+        self._dpadLeft = KeyboardButtonInputs(name="D-Pad Left")
+        self._dpadRight = KeyboardButtonInputs(name="D-Pad Right")
         
-        row2.addWidget(dpadUp)
+        row2.addWidget(self._dpadUp)
         row2.addSpacing(columnSpacing)
-        row2.addWidget(dpadDown)
+        row2.addWidget(self._dpadDown)
         row2.addSpacing(columnSpacing)
-        row2.addWidget(dpadLeft)
+        row2.addWidget(self._dpadLeft)
         row2.addSpacing(columnSpacing)
-        row2.addWidget(dpadRight)
+        row2.addWidget(self._dpadRight)
+        
+        # Row 3
+        row3 = wdgts.NoPadHBoxLayout()
+        self._select = KeyboardButtonInputs(name="Select")
+        self._start = KeyboardButtonInputs(name='Start')
+        
+        row3.addWidget(self._select)
+        row3.addSpacing(columnSpacing)
+        row3.addWidget(self._start)
+        
         
         # Config Manager
         configManager = ConfigManager(console=self)
@@ -74,18 +82,28 @@ class Gameboy(QFrame):
         mainLayout.addLayout(row1)
         mainLayout.addSpacing(columnSpacing)
         mainLayout.addLayout(row2)
+        mainLayout.addSpacing(columnSpacing)
+        mainLayout.addLayout(row3)
         mainLayout.addSpacing(20)
         mainLayout.addWidget(configManager)
         mainLayout.addStretch(True)
 
     def get_current_inputs(self) -> dict:
         return {
-            
+            'a': self._buttonA.get_inputs(),
+            'b': self._buttonB.get_inputs(),
+            'r': self._bumperR.get_inputs(),
+            'l': self._bumperR.get_inputs(),
+            'dpad_up': self._dpadUp.get_inputs(),
+            'dpad_down': self._dpadDown.get_inputs(),
+            'dpad_left': self._dpadLeft.get_inputs(),
+            'dpad_right': self._dpadRight.get_inputs(),
         }
 
 
+
 class ConfigManager(QFrame):
-    def __init__(self, console):
+    def __init__(self, console: Gameboy):
         super().__init__()
         self.console = console
         self.comboButtonPopup = popups.ButtonCombosConfig(self)
@@ -109,7 +127,6 @@ class ConfigManager(QFrame):
         # Button Combos Buttons
         comboButton = QPushButton(text='Open Button Combos')
         comboButton.setFixedWidth(200)
-        comboButton.clicked.connect(self.open_button_combos)
         
         buttonsContainer.addWidget(newButton)
         buttonsContainer.addSpacing(10)
@@ -119,13 +136,32 @@ class ConfigManager(QFrame):
         mainLayout.addSpacing(4)
         mainLayout.addLayout(buttonsContainer)
         mainLayout.addWidget(comboButton, alignment=gui.ALIGN_CENTER)
+        
+        newButton.clicked.connect(self.save_new_preset)
+        updateButton.clicked.connect(self.update_preset)
+        comboButton.clicked.connect(self.open_button_combos)
 
     @Slot()
-    def open_button_combos(self):
+    def open_button_combos(self) -> None:
         if not self.comboButtonPopup.isVisible():
             self.comboButtonPopup.exec()
         else:
             return
+
+    @Slot()
+    def save_new_preset(self) -> None:
+        name, ok = QInputDialog.getText(self, "New Preset", "Preset Name")
+        if ok:
+            controls = self.console.get_current_inputs()
+            preset = {
+                name: controls
+            }
+            
+            
+    
+    @Slot()
+    def update_preset(self, preset: str) -> None:
+        pass
 
 class KeyboardButtonInputs(QFrame):
     def __init__(self, *, name: str):
